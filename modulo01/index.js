6f2e94f5709ec3f5b6d0df5841e85d3a0d6fb0b5
Daniel Vieira <contatodanielnvieira@outlook.com>
2020-07-23 23:35:04 -0300

    Criando as rotas iniciais do desafio

--- modulo01/index.js
+++ modulo01/index.js
@@ -58,6 +58,7 @@ server.get('/users/:index', checkUserInArray, (req, res) =>{
 server.post('/users', checkUserExists, (req, res) =>{
   const {name} = req.body
   users.push(name)
+  
   return res.json(users)
 })
 
---
+++ modulo01_desafio/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+const projetos = []
+
+
+
+server.get('/projects', (req, res) =>{
+  res.json(projetos)
+})
+
+
+server.post('/projects', (req, res) =>{
+  const {id, title} = req.body
+  
+  projetos.push({
+    id,
+    title,
+    tasks: []
+  })
+
+  return res.json(projetos)
+})
+
+
+
+server.get('/projects/:id', (req, res) =>{
+  const {id} = req.params
+
+  return res.json(projetos[id - 1])
+})
+
+
+
+server.put('/projects/:id', (req, res) =>{
+  const {title} = req.body
+  const {id} = req.params
+
+  projetos[id -1].title = title
+
+  return res.json(projetos)
+})
+
+
+
+server.listen(3000, () =>{
+  console.log("servidor rodando na porta 3000")
+})
