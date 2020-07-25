119c4e53be7d627ecb4e10b0addf113ccdbcda4e
Daniel Vieira <contatodanielnvieira@outlook.com>
2020-07-25 19:16:46 -0300

    Criando a base a aplicação

---
+++ modulo02/src/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const routes = require('./routes')
+
+class App{
+
+  // constructors iniciam automaticamente quando a classe é chamada
+  constructor(){
+    //é a mesma coisa que app = express()
+    this.server = express()
+
+
+    this.middlewares()
+
+    this.routes()
+  }
+
+  //Aqui vão todos os middlewares da aplicação
+
+  middlewares(){
+    this.server.use(express.json())
+  }
+
+  routes(){
+    this.server.use(routes)
+  }
+}
+
+module.exports = new App().server
