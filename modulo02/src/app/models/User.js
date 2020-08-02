dd2e4603a2b731517a550a09b07af7799982cf7d
Daniel Vieira <contatodanielnvieira@outlook.com>
2020-08-02 03:47:17 -0300

    Gerando o hash de senha

--- modulo02/src/app/models/User.js
+++ modulo02/src/app/models/User.js
@@ -1,11 +1,14 @@
 import { Sequelize, Model } from 'sequelize';
 
+import bcrypt from 'bcrypt';
+
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -13,6 +16,14 @@ class User extends Model {
         sequelize,
       }
     );
+    // Hooks são trechos de códigos que são executados de forma automática baseado em ações que acontecem no nosso model.
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10);
+      }
+    });
+
+    return this;
   }
 }
 
